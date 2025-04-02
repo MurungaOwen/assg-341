f6f3f800b2958738fd3c66d94e6090f63635a7d3
MurungaOwen <owenhood80@gmail.com>
2025-04-02 12:08:22 +0300

    fix: correct path to my view file

---
+++ src/java/com/Trips/controller/ListTrips.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.Trips.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ *
+ * @author hood
+ */
+public class ListTrips extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws IOException, ServletException{
+     req.getRequestDispatcher("/WEB-INF/views/bookings/list.jsp").forward(req, res);
+    }
+}
